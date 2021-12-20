b646eac1aba703df938fce129a29232a8b76f1bd
ShmuelLa <shmuel516@gmail.com>
2021-12-20 21:08:32 +0200

    added options to TraderActivity

--- app/src/main/java/com/example/cryptotrader/TraderActivity.java
+++ app/src/main/java/com/example/cryptotrader/TraderActivity.java
@@ -4,9 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,7 +18,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -25,6 +27,11 @@ public class TraderActivity extends AppCompatActivity implements View.OnClickLis
     private String[] tradeOptions = {"Buy", "Sell", "Limit Buy", "Limit Sell", "Futures Buy", "Futures Sell"};
     private String[] symbolFundOptions = {"USDT", "BUSD", "BNB"};
     private String[] symbolTargetOptions = {"BTC", "ETH", "ADA", "MANA", "BNB"};
+    private Spinner clientSpinner;
+    private Spinner tradeOptionsSpinner;
+    private Spinner symbolFundSpinner;
+    private Spinner symbolTargetSpinner;
+    ArrayList<String> clientsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +39,15 @@ public class TraderActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_trader);
         user = FirebaseAuth.getInstance().getCurrentUser();
         accounts_db = FirebaseDatabase.getInstance().getReference("Accounts");
-        Spinner clientDropdown = findViewById(R.id.clientSpinner);
-        Spinner tradeOptionsDropdown = findViewById(R.id.optionsSpinner);
-        Spinner symbolFundDropdown = findViewById(R.id.symbolFundSpinner);
-        Spinner symbolTargetDropdown = findViewById(R.id.symbolTargetSpinner);
-        ArrayList<String> clientsList = new ArrayList<>();
+        clientSpinner = findViewById(R.id.clientSpinner);
+        tradeOptionsSpinner = findViewById(R.id.optionsSpinner);
+        symbolFundSpinner = findViewById(R.id.symbolFundSpinner);
+        symbolTargetSpinner = findViewById(R.id.symbolTargetSpinner);
         clientsList.add("All");
         accounts_db.child(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(task.isSuccessful()){
+                if(task.isSuccessful()) {
                     GenericTypeIndicator<HashMap<String, ctCredentials>> gType = new GenericTypeIndicator<HashMap<String,  ctCredentials>>() {};
                     HashMap<String,  ctCredentials> map = task.getResult().getValue(gType);
                     map.forEach((clientName, clientTokens) -> clientsList.add(clientName));
@@ -52,10 +58,11 @@ public class TraderActivity extends AppCompatActivity implements View.OnClickLis
         ArrayAdapter<String> tradeOptionsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, tradeOptions);
         ArrayAdapter<String> symbolFundsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, symbolFundOptions);
         ArrayAdapter<String> symbolTargetAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, symbolTargetOptions);
-        clientDropdown.setAdapter(clientsAdapter);
-        tradeOptionsDropdown.setAdapter(tradeOptionsAdapter);
-        symbolFundDropdown.setAdapter(symbolFundsAdapter);
-        symbolTargetDropdown.setAdapter(symbolTargetAdapter);
+        clientSpinner.setAdapter(clientsAdapter);
+        tradeOptionsSpinner.setAdapter(tradeOptionsAdapter);
+        symbolFundSpinner.setAdapter(symbolFundsAdapter);
+        symbolTargetSpinner.setAdapter(symbolTargetAdapter);
+//        spinnerColletion(clientSpinner, tradeOptionsSpinner, symbolFundSpinner, symbolTargetSpinner);
     }
 
     @Override
@@ -72,5 +79,29 @@ public class TraderActivity extends AppCompatActivity implements View.OnClickLis
                 }
                 break;
         }*/
+    }
+
+//    public void spinnerColletion(Spinner first, Spinner second, Spinner third, Spinner fourth) {
+//        first.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                TextView textView = (TextView)first.getSelectedView();
+//                switch(textView.getText().toString()) {
+//
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+//    }
+
+    public String getSpinnerChosenText(Spinner spinner) {
+        TextView textView = (TextView)spinner.getSelectedView();
+        String result = textView.getText().toString();
+        return result;
     }
 }
