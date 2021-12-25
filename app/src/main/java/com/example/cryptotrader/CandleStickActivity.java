ad4d539228acfb8d70271fed7e86d1e743fc961e
ShmueLa <shmuel516@gmail.com>
2021-12-25 19:56:24 +0200

    Added CoinGeckoAPI library

--- app/src/main/java/com/example/cryptotrader/CandleStickActivity.java
+++ app/src/main/java/com/example/cryptotrader/CandleStickActivity.java
@@ -14,6 +14,18 @@ import com.binance.api.client.BinanceApiWebSocketClient;
 import com.binance.api.client.domain.market.Candlestick;
 import com.binance.api.client.domain.market.CandlestickInterval;
 
+import com.litesoftwares.coingecko.CoinGeckoApiClient;
+import com.litesoftwares.coingecko.constant.Currency;
+import com.litesoftwares.coingecko.domain.Coins.CoinData.DeveloperData;
+import com.litesoftwares.coingecko.domain.Coins.CoinData.IcoData;
+import com.litesoftwares.coingecko.domain.Coins.CoinFullData;
+import com.litesoftwares.coingecko.domain.Coins.CoinList;
+import com.litesoftwares.coingecko.domain.Coins.CoinMarkets;
+import com.litesoftwares.coingecko.domain.Coins.CoinTickerById;
+import com.litesoftwares.coingecko.impl.CoinGeckoApiClientImpl;
+
+import java.util.List;
+
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -26,102 +38,54 @@ public class CandleStickActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_candel_stick);
-//        new CandleStickTask("BTCUSDT", CandlestickInterval.DAILY).execute();
-        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance();
-        BinanceApiRestClient client = factory.newRestClient();
-        client.getCandlestickBars("BTCUSDT", CandlestickInterval.DAILY);
+        new CandleStickTask().execute();
     }
 
     private class CandleStickTask extends AsyncTask<String, Void, String> {
-        private Map<Long, Candlestick> candlesticksCache;
-        String symbolToExecute;
-        CandlestickInterval intervalToExecute;
 
-        public CandleStickTask(String symbol, CandlestickInterval interval) {
-            this.symbolToExecute = symbol;
-            this.intervalToExecute = interval;
+        public CandleStickTask() {
         }
 
-        /**
-         * Initializes candlesticks by using the Binance REST API.
-         */
-        private void initializeCandlestickCache(String symbol, CandlestickInterval interval) {
-
-
-
-
-
-//            BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance();
-//            BinanceApiAsyncRestClient client = factory.newAsyncRestClient();
-//            client.getCandlestickBars(symbol.toUpperCase(), interval, new BinanceApiCallback<List<Candlestick>>() {
-//                @Override
-//                public void onResponse(List<Candlestick> candlesticks) {
-//                    System.out.println("zibi");
-//                    for (Candlestick candle : candlesticks) {
-//                        System.out.println(candle.toString());
-//                    }
-//                }
-//
-//                @Override
-//                public void onFailure(Throwable cause) {
-//                    System.out.println("zababir");
-//                    System.out.println(cause.toString());
-//                }
-//            });
-
-
-
-
-//            this.candlesticksCache = new TreeMap<>();
-//            for (Candlestick candlestickBar : candlestickBars) {
-//                candlesticksCache.put(candlestickBar.getOpenTime(), candlestickBar);
-//            }
-        }
-
-//        /**
-//         * Begins market data streaming
-//         */
-//        private void startCandlestickEventStreaming(String symbol, CandlestickInterval interval) {
-//            BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance();
-//            BinanceApiWebSocketClient client = factory.newWebSocketClient();
-//
-//            client.onCandlestickEvent(symbol.toLowerCase(), interval, response -> {
-//                Long openTime = response.getOpenTime();
-//                Candlestick updateCandlestick = candlesticksCache.get(openTime);
-//                if (updateCandlestick == null) {
-//                    // new candlestick
-//                    updateCandlestick = new Candlestick();
-//                }
-//                // update candlestick with the stream data
-//                updateCandlestick.setOpenTime(response.getOpenTime());
-//                updateCandlestick.setOpen(response.getOpen());
-//                updateCandlestick.setLow(response.getLow());
-//                updateCandlestick.setHigh(response.getHigh());
-//                updateCandlestick.setClose(response.getClose());
-//                updateCandlestick.setCloseTime(response.getCloseTime());
-//                updateCandlestick.setVolume(response.getVolume());
-//                updateCandlestick.setNumberOfTrades(response.getNumberOfTrades());
-//                updateCandlestick.setQuoteAssetVolume(response.getQuoteAssetVolume());
-//                updateCandlestick.setTakerBuyQuoteAssetVolume(response.getTakerBuyQuoteAssetVolume());
-//                updateCandlestick.setTakerBuyBaseAssetVolume(response.getTakerBuyQuoteAssetVolume());
-//
-//                // Store the updated candlestick in the cache
-//                candlesticksCache.put(openTime, updateCandlestick);
-//                System.out.println(updateCandlestick);
-//            });
-//        }
-//
-//        /**
-//         * @return a klines/candlestick cache, containing the open/start time of the candlestick as the key,
-//         * and the candlestick data as the value.
-//         */
-//        public Map<Long, Candlestick> getCandlesticksCache() {
-//            return candlesticksCache;
-//        }
-//
         @Override
         protected String doInBackground(String... strings) {
-            initializeCandlestickCache(this.symbolToExecute, this.intervalToExecute);
+            String OMGContract = "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07";
+            String platform = "ethereum";
+
+            CoinGeckoApiClient client = new CoinGeckoApiClientImpl();
+
+            List<CoinList> coinList = client.getCoinList();
+            System.out.println(coinList);
+
+            long totalCoins = coinList.size();
+            System.out.println(totalCoins);
+
+            List<CoinMarkets> coinMarkets = client.getCoinMarkets(Currency.USD);
+            System.out.println(coinMarkets);
+
+            CoinFullData bitcoinInfo = client.getCoinById("bitcoin");
+            System.out.println(bitcoinInfo);
+
+            String genesisDate = bitcoinInfo.getGenesisDate();
+            System.out.println(genesisDate);
+
+
+            DeveloperData bitcoinDevData = bitcoinInfo.getDeveloperData();
+            System.out.println(bitcoinDevData);
+
+            long bitcoinGithubStars = bitcoinDevData.getStars();
+            System.out.println(bitcoinGithubStars);
+
+            CoinTickerById bitcoinTicker = client.getCoinTickerById("bitcoin");
+            System.out.println(bitcoinTicker);
+
+            CoinFullData omiseGoInfo = client.getCoinInfoByContractAddress(platform, OMGContract);
+            System.out.println(omiseGoInfo);
+
+            IcoData omiseGoIcoInfo = omiseGoInfo.getIcoData();
+            String icoStartDate = omiseGoIcoInfo.getIcoStartDate();
+            System.out.println(icoStartDate);
+
+            client.shutdown();
             return null;
         }
     }
---
+++ app/src/main/java/com/example/cryptotrader/CandleStickDataReposiory.java
@@ -0,0 +1,8 @@
+//package com.example.cryptotrader;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface CandleStickDataReposiory extends CrudRepository<CandleStickData,Long>{
+//}
