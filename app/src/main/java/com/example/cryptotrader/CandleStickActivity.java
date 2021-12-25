43596c55f0f8e57a358533fcd6038c02446589f4
ShmueLa <shmuel516@gmail.com>
2021-12-25 22:51:54 +0200

    Added MPChart Library

--- app/src/main/java/com/example/cryptotrader/CandleStickActivity.java
+++ app/src/main/java/com/example/cryptotrader/CandleStickActivity.java
@@ -22,6 +22,7 @@ import com.litesoftwares.coingecko.domain.Coins.CoinFullData;
 import com.litesoftwares.coingecko.domain.Coins.CoinList;
 import com.litesoftwares.coingecko.domain.Coins.CoinMarkets;
 import com.litesoftwares.coingecko.domain.Coins.CoinTickerById;
+import com.litesoftwares.coingecko.domain.Coins.MarketChart;
 import com.litesoftwares.coingecko.impl.CoinGeckoApiClientImpl;
 
 import java.util.List;
@@ -41,50 +42,13 @@ public class CandleStickActivity extends AppCompatActivity {
         new CandleStickTask().execute();
     }
 
-    private class CandleStickTask extends AsyncTask<String, Void, String> {
-
-        public CandleStickTask() {
-        }
+    private static class CandleStickTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... strings) {
-            String OMGContract = "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07";
-            String platform = "ethereum";
-
             CoinGeckoApiClient client = new CoinGeckoApiClientImpl();
-
-            List<CoinList> coinList = client.getCoinList();
-            System.out.println(coinList);
-
-            long totalCoins = coinList.size();
-            System.out.println(totalCoins);
-
-            List<CoinMarkets> coinMarkets = client.getCoinMarkets(Currency.USD);
-            System.out.println(coinMarkets);
-
-            CoinFullData bitcoinInfo = client.getCoinById("bitcoin");
-            System.out.println(bitcoinInfo);
-
-            String genesisDate = bitcoinInfo.getGenesisDate();
-            System.out.println(genesisDate);
-
-
-            DeveloperData bitcoinDevData = bitcoinInfo.getDeveloperData();
-            System.out.println(bitcoinDevData);
-
-            long bitcoinGithubStars = bitcoinDevData.getStars();
-            System.out.println(bitcoinGithubStars);
-
-            CoinTickerById bitcoinTicker = client.getCoinTickerById("bitcoin");
-            System.out.println(bitcoinTicker);
-
-            CoinFullData omiseGoInfo = client.getCoinInfoByContractAddress(platform, OMGContract);
-            System.out.println(omiseGoInfo);
-
-            IcoData omiseGoIcoInfo = omiseGoInfo.getIcoData();
-            String icoStartDate = omiseGoIcoInfo.getIcoStartDate();
-            System.out.println(icoStartDate);
-
+            MarketChart marketchart = client.getCoinMarketChartById("bitcoin","usd",1);
+            System.out.println(marketchart.toString());
             client.shutdown();
             return null;
         }
